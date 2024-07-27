45080f5134267d6dca61ee641b6b585d622fdd00
Souvik3798 <souvikbarua693@gmail.com>
2024-07-27 23:31:45 +0530

    "Updated import statement in ItemResource.php to use correct case for 'Item' model."

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ItemResource\Pages;
 use App\Filament\Resources\ItemResource\RelationManagers;
-use App\Models\item;
+use App\Models\Item;
 use Filament\Forms;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
