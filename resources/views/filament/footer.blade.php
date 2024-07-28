d97cc673b4395d18d2a4c05b06742a2154c382e2
Souvik3798 <souvikbarua693@gmail.com>
2024-07-28 16:27:59 +0530

    target

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -1,6 +1,6 @@
 <footer
     style="background-color: rgb(185, 2, 2); color: white; position: fixed; bottom: 0; width: 100%; text-align: center;">
     <div class="container mx-auto text-center">
-        <p>Powered by <a href="https://unicorniz.com/">Unicorniz</a> @ {{ date('Y') }}</p>
+        <p>Powered by <a href="https://unicorniz.com/" target="_blank">Unicorniz</a> @ {{ date('Y') }}</p>
     </div>
 </footer>
