b5f0ad4130d9279e70bef8cdc7aa37ff46f80d74
Souvik3798 <souvikbarua693@gmail.com>
2024-08-01 20:32:41 +0530

    "Added tabs to ListRecords pages for Dish, Inventory, Item, and Sale resources with filters for all, today, this week, and this month"

--- app/Filament/Resources/InventoryResource/Pages/ListInventories.php
+++ app/Filament/Resources/InventoryResource/Pages/ListInventories.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\InventoryResource\Pages;
 use App\Filament\Resources\InventoryResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class ListInventories extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListInventories extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'today' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDate('updated_at', Carbon::today())),
+            'this_week' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereBetween('updated_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])),
+            'this_month' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereMonth('updated_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)),
         ];
     }
 }
--- app/Filament/Resources/ItemResource/Pages/ListItems.php
+++ app/Filament/Resources/ItemResource/Pages/ListItems.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\ItemResource\Pages;
 use App\Filament\Resources\ItemResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class ListItems extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListItems extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'today' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDate('updated_at', Carbon::today())),
+            'this_week' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereBetween('updated_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])),
+            'this_month' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereMonth('updated_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year)),
         ];
     }
 }
